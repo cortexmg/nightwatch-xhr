446cd73956f70da7518ba47928ea3007add2da05
Jalil Arfaoui <jalil@arfaoui.net>
2017-09-30 22:14:58 +0200

    Add es5 transpile script

---
+++ es5/assertions/isXHRSuccess.js
@@ -0,0 +1,19 @@
+String.prototype.trunc = String.prototype.trunc || function (n) {
+    return this.length > n ? this.substr(0, n - 1) + '...' : this;
+};
+
+exports.assertion = function XHRSuccess(listenedXhr) {
+    this.message = `Expected ${listenedXhr.url.trunc(30)} to called successfully`;
+    this.expected = 'success';
+    this.pass = function (value) {
+        return value === this.expected;
+    };
+    this.value = function (result) {
+        return result.status;
+    };
+    this.command = function command(callback) {
+        const self = this;
+        setImmediate(() => callback.call(self, listenedXhr));
+        return this;
+    };
+};
